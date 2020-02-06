184995aff3a64f5d82a023aad98ffde26ce09630
Shamamhabuba <shamamhabuba@gmail.com>
2020-02-06 22:26:36 +0200

    div

--- src/app/movies/movies-list/movies-list.component.spec.ts
+++ src/app/movies/movies-list/movies-list.component.spec.ts
@@ -1,22 +1,57 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { of } from 'rxjs';
+import { MovieClientService } from 'src/app/shared/movie-client.service';
+import { MovieListComponent } from './movie-list.component';
 
-import { MoviesListComponent } from './movies-list.component';
-
-describe('MoviesListComponent', () => {
-  let component: MoviesListComponent;
-  let fixture: ComponentFixture<MoviesListComponent>;
+describe('MovieListComponent', () => {
+  let component: MovieListComponent;
+  let fixture: ComponentFixture<MovieListComponent>;
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate'),
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MoviesListComponent ]
-    })
-    .compileComponents();
+      declarations: [MovieListComponent],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        NgxPaginationModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        MovieClientService,
+        {
+          provide: Router,
+          useValue: mockRouter,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({ s: 's', page: '1' }),
+          },
+        },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MoviesListComponent);
+    fixture = TestBed.createComponent(MovieListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 
   it('should create', () => {
